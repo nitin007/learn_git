9a16bd07f6b9f7208a30bcf9b715b02b12f050b9
Nitin Gupta <nitinmangala@in.com>
2012-07-30 17:42:26 +0530

    my initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnGit::Application.config.secret_token = '54e0318d821a7e0190e536a29cdd1fba18df65dbf710efb4c7df3c639b22156ed882a7f6dbb56f50578c37536689ef6d4909627725114a51c45d8f0c559ebf86'
